ac87872b851d2af61bfce8801765381b7c84424b
muadhib2001 <40434397+muadhib2001@users.noreply.github.com>
2025-12-30 02:20:37 +0100

    ok

--- Turkish/src/main/kotlin/com/hexated/Turkish.kt
+++ Turkish/src/main/kotlin/com/hexated/Turkish.kt
@@ -150,7 +150,7 @@ class Turkish : MainAPI() {
                 val html=e.outerHtml()
                 
                 Log.i("#sarem", html);
-                //val slist=Regex("<iframe[^=]*src=[\"|']([^=]*)[\"|']").findAll(html).map { it.groupValues[1] }.toList()
+                //val slist=Regex("<iframe.*src=[\"|'](https[^\"]*)[\"|']").findAll(html).map { it.groupValues[1] }.toList()
                 val slist=Regex("[^=]*src=[\"|']([^=]*)[\"|']").findAll(html).map { it.groupValues[1] }.toList()
                 val size = slist.size
                 Log.i("#sarem222222#",slist.joinToString())
